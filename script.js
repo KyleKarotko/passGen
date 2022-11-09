2c0a3f622c40db4a074732052d69a8f87cac5038
kyle_karotko <kkrtk7@duck.com>
2022-11-08 21:17:31 -0500

    added prompts for character types and length

--- script.js
+++ script.js
@@ -1,3 +1,126 @@
+// Array of special characters to be included in password
+var specialCharacters = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+
+// Array of numeric characters to be included in password
+var numericCharacters = [
+  '0', 
+  '1', 
+  '2', 
+  '3', 
+  '4', 
+  '5', 
+  '6', 
+  '7', 
+  '8', 
+  '9',
+];
+
+// Array of lowercase characters to be included in password
+var lowerCasedCharacters = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+
+// Array of uppercase characters to be included in password
+var upperCasedCharacters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+
+//prompt asking how many characters the user would like to use
+document.querySelector("#generate").addEventListener('click', function() {
+  var length = prompt("How many characters would you like your passwrod to be(must be between 8 and 128 characters)?");
+  if(length < 8) {
+    alert("Must be at least 8 characters!");
+    return;
+  }
+  
+  if(length > 128) {
+    alert("must be less than 128 characters");
+  }
+//prompts asking what kind of characters the user would like to have in their password
+  var useLowercase = confirm("Would you like to use lowercase characters?");
+  var useUppercase = confirm("Would you like to use uppercase characters?");
+  var useSpecialCharacters = confirm("Would you like to use special characters?");
+
+
+  
+});
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
